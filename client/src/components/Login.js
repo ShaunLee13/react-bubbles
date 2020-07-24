259555585d8e4927ac68c2f29a02aee17d0adede
Shaun Lee <shaundlee.0103@gmail.com>
2020-07-24 12:03:07 -0400

    Private Route set up; Data retrieval on component mount

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from 'axios'
+import { useHistory } from 'react-router-dom'
 
 const initialLogin = {
   username:'',
@@ -9,16 +10,19 @@ const initialLogin = {
 
 const Login = () => {
   const [login, setLogin] = useState(initialLogin)
+  const { push } = useHistory()
+
 
   const handleChange = e => {
     setLogin({...login, [e.target.name]:e.target.value})
   }
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
   const loginSubmit = e => {
     e.preventDefault()
     axios.post('http://localhost:5000/api/login', login)
-      .then(res => console.log(res))
+      .then(res => {
+        window.localStorage.setItem('token', res.data.payload)
+        push('/bubblepage')
+      })
       .catch(err => console.log(err))
 
     }
